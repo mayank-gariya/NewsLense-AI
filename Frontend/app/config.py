b8984a02f63f25fe62cd5e131f0d3f25734e4521
mayank-ml_dev <albert.creates85@gmail.com>
2026-07-12 22:35:00 +0530

    Update config.py

--- Frontend/app/config.py
+++ Frontend/app/config.py
@@ -1,9 +1,9 @@
-# config.py
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class Config:
-    GNEWS_API_KEY = os.getenv("GNEWS_API_KEY", ""),
-    BASE_URL = os.getenv('BASE_URL','')
+    BASE_URL = st.secrets.get("BASE_URL", os.getenv("BASE_URL", ""))
+    GNEWS_API_KEY = st.secrets.get("GNEWS_API_KEY", os.getenv("GNEWS_API_KEY", ""))
